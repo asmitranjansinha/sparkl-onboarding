63f20ffc1d242a4c2c0bd371a1240db77d38ac5c
asmy8h <sinhaasmit99@gmail.com>
2025-02-27 19:36:39 +0530

    :building_construction: app app_assets

---
+++ lib/core/config/app_assets.dart
@@ -0,0 +1,19 @@
+class AppAssets {
+  /// Images
+  static const _image = "assets/images";
+  static const String blueBook = "$_image/blue_book.png";
+  static const String emoji = "$_image/emoji.png";
+  static const String preReadSelected = "$_image/pre_read_selected.png";
+  static const String sparklLogo = "$_image/sparkl_logo.png";
+  static const String stackCard = "$_image/stack_card.png";
+
+  /// Videos
+  static const _video = "assets/videos";
+  static const String studentVideo = "$_video/studentvideo.mp4";
+  static const String teachervideo = "$_video/teachervideo.mp4";
+
+  /// Lottie
+  static const _lottie = "assets/lotties";
+  static const String sparklShapeShiftLottie =
+      "$_lottie/sparkl_shape_shift_lottie.json";
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:sprkl_onboarding/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const SparklOnboarding());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
